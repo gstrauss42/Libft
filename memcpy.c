d3cfee272b3d425cc04a88c5de77736edd020c24
Gabriel Strauss <gstrauss@c4r16s4.wethinkcode.co.za>
2019-05-21 09:51:33 +0200

    first 3 done, just need to check if malloc is needed for mem functions and then testing needed

--- memcpy.c
+++ memcpy.c
@@ -1,3 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
 void *ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
 {
+	int i;
+	char *dest, *source;
 
+	dest = (char *)dst;
+	source= (char *)src;
+	i = 0;
+	while(i < n)
+	{
+		dest[i] = source[i];
+		i++;
+	}
+	return(dest);
+}
+
+int main()
+{
+	char src[12] = "Hello There";
+	char dst[12] = "abcdefghijk";
+	printf("%s", ft_memcpy(dst, src, 4 * sizeof(char)));
+	return(0);
+}
