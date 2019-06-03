956429023256d4d89e19a3f68ac51570e1500301
Gabriel Strauss <gstrauss@c5r4s1.wethinkcode.co.za>
2019-06-03 09:47:07 +0200

    fixed itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,31 +6,47 @@
 /*   By: gstrauss <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/28 08:46:57 by gstrauss          #+#    #+#             */
-/*   Updated: 2019/05/31 13:40:11 by gstrauss         ###   ########.fr       */
+/*   Updated: 2019/06/03 09:46:33 by gstrauss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 char *	ft_itoa(int n)
 {
 	char * ret;
-	ret = 0;
 	int i;
-	int z;
-	int c;
-	c = 0;
-	z = 0;
+	int a;
+	int q;
+	int o;
+	o = 0;
+	a = 0;
+	q = 0;
 	i = 1;
 	while(n / i != 0)
 	{
+		q++;
 		i = i * 10;
-		c++;
+	}
+	ret = (char *)malloc((q + 2) * sizeof(char));
+	if(ret == NULL)
+		return(NULL);
+	if( n == 0)
+	{
+		ret[0] = 48;
+		ret[1] = '\0';
+		return(ret);
 	}
 	i = i / 10;
-	while(c > 0)
+	while( q + 1  > 0)
 	{
-		ret[z] = n / i;
-		n = n - ((n / i) * i);
-		c--;
+		a = n / i;
+		ret[o] = a + 48;
+		a = a * i;
+		i = i / 10;
+		n = n - a;
+		q--;
+		o++;
 	}
 	return(ret);
 }
