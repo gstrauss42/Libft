34106cd8aea4b12d000148e3b5c81dea243bb799
Gabriel Strauss <gstrauss@c4r18s1.wethinkcode.co.za>
2019-06-05 13:11:28 +0200

    norm done I think

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,47 +6,58 @@
 /*   By: gstrauss <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/28 08:46:57 by gstrauss          #+#    #+#             */
-/*   Updated: 2019/06/03 09:46:33 by gstrauss         ###   ########.fr       */
+/*   Updated: 2019/06/05 13:10:49 by gstrauss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *	ft_itoa(int n)
+static char	*test1(char *ret)
 {
-	char * ret;
+	ret[0] = 48;
+	ret[1] = '\0';
+	return (ret);
+}
+
+static int	length(int n)
+{
 	int i;
-	int a;
 	int q;
-	int o;
-	o = 0;
-	a = 0;
+
 	q = 0;
 	i = 1;
-	while(n / i != 0)
+	while (n / i != 0)
 	{
 		q++;
 		i = i * 10;
 	}
-	ret = (char *)malloc((q + 2) * sizeof(char));
-	if(ret == NULL)
-		return(NULL);
-	if( n == 0)
+	return (q);
+}
+
+char		*ft_itoa(int n)
+{
+	char	*ret;
+	int		i[4];
+
+	i[0] = 0;
+	i[1] = 0;
+	i[2] = length(n);
+	i[3] = 1;
+	while (n / (i[3] * 10) != 0)
+		i[3] = i[3] * 10;
+	if (!(ret = (char *)malloc((length(n) + 1) * sizeof(char))))
+		return (NULL);
+	if (n == 0)
+		return (test1(ret));
+	while (i[2] + 1 > 0)
 	{
-		ret[0] = 48;
-		ret[1] = '\0';
-		return(ret);
+		i[1] = n / i[3];
+		ret[i[0]] = i[1] + 48;
+		i[1] = i[1] * i[3];
+		i[3] = i[3] / 10;
+		n = n - i[1];
+		i[2]--;
+		i[0]++;
 	}
-	i = i / 10;
-	while( q + 1  > 0)
-	{
-		a = n / i;
-		ret[o] = a + 48;
-		a = a * i;
-		i = i / 10;
-		n = n - a;
-		q--;
-		o++;
-	}
-	return(ret);
+	return (ret);
 }
