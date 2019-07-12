81b4451c2d3d260b9177d058703d14b43df54237
Gabriel Strauss <gstrauss@c5r5s4.wethinkcode.co.za>
2019-07-12 12:50:55 +0200

    stuff

--- ft_lstezmake.c
+++ ft_lstezmake.c
@@ -6,12 +6,11 @@
 /*   By: gstrauss <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/12 12:03:43 by gstrauss          #+#    #+#             */
-/*   Updated: 2019/07/12 12:44:38 by gstrauss         ###   ########.fr       */
+/*   Updated: 2019/07/12 12:50:48 by gstrauss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 t_list	*ft_lstezmake(t_list **head, char *input)
 {
@@ -20,23 +19,4 @@ t_list	*ft_lstezmake(t_list **head, char *input)
 	else
 		ft_lstend(*head, ft_lstnew(input, ft_strlen(input) + 1));
 	return(*head);
-}
-
-int main(int argc, char **argv)
-{
-	t_list *head;
-	int i;
-
-	i = 0;
-	while(i<argc)
-	{
-		head = ft_lstezmake(&head, argv[i]); 
-		i++;
-	}
-	while(head)
-	{
-		printf("%s\n", head->content);
-		head = head->next;
-	}
-	return(0);
 }
