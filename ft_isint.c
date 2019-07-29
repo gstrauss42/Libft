189f105134f2790eb003e7d48c6501d57ce8c901
Gabriel Strauss <gstrauss@c5r5s4.wethinkcode.co.za>
2019-07-29 11:35:57 +0200

    got ft_isint working

--- ft_isint.c
+++ ft_isint.c
@@ -6,31 +6,62 @@
 /*   By: gstrauss <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/26 12:56:22 by gstrauss          #+#    #+#             */
-/*   Updated: 2019/07/26 13:56:01 by gstrauss         ###   ########.fr       */
+/*   Updated: 2019/07/29 11:35:31 by gstrauss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static int check(char *tmp, char *str)
+{
+	int p = 0;
+	if(str[0] == '-')
+	{
+		char ttmp[11] = "-2147483648";
+		while(str[p])
+		{
+			if(!ttmp[p] || str[p] > ttmp[p])
+				return(0);
+			else if(ttmp[p] == str[p])
+				p++;
+			else if(ttmp[p] < str[p])
+				break;
+		}
+	}
+	else
+	{
+		while(str[p])
+		{
+			if(!tmp[p] || str[p] > tmp[p])
+				return(0);
+			else if(tmp[p] == str[p])
+				p++;
+			else if(tmp[p] < str[p])
+				break;
+		}
+	}
+	return(1);
+}
+
 int		ft_isint(char *str)
 {
 	int i;
 	i = 0;
-
-	if(ft_strlen(str) > 10)
-		return(0)
+	char tmp[11] = "2147483647";
 	if(str[i] == '-')
 		i++;
+	if(ft_strlen(str) > 10 && str[0] != '-')
+		return(0);
+	if(ft_strlen(str) > 11 && str[0] == '-')
+		return(0);
 	while(str[i])
 	{
 		if(ft_isdigit(str[i]) == 1)
 			i++;
 		else
-			break;
+			return(0);
 	}
-	if(str[i])
+	if(check(tmp, str) == 0)
 		return(0);
-	else if(!str[i])
-		return(1);
-	return(0);
+	return(1);
 }
