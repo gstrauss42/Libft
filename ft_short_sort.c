04b53237c6715f7f4749195cc723140ec965f9e3
Gabriel Strauss <gstrauss@c4r18s3.wethinkcode.co.za>
2019-08-30 17:15:21 +0200

    stuff

--- ft_short_sort.c
+++ ft_short_sort.c
@@ -6,12 +6,13 @@
 /*   By: gstrauss <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/30 09:05:47 by gstrauss          #+#    #+#             */
-/*   Updated: 2019/08/30 13:53:38 by gstrauss         ###   ########.fr       */
+/*   Updated: 2019/08/30 17:14:53 by gstrauss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+void	last(t_list **lista, t_list **listb);
 void	n3(t_list **lista);
 void	n5(t_list **lista, t_list **listb);
 
@@ -63,6 +64,11 @@ void	n5(t_list **lista, t_list **listb)
 			write(1, "sa\n", 3);
 		}
 	}
+	else
+	{
+		last(lista, listb);
+		last(lista, listb);
+	}
 }
 
 void	n3(t_list **lista)
@@ -83,3 +89,43 @@ void	n3(t_list **lista)
 		write(1, "sa\n", 3);
 	}
 }
+
+void	last(t_list **lista, t_list **listb)
+{
+	if((*listb)->fpos < (*lista)->fpos)
+	{
+		ft_pa(lista, listb);
+		write(1, "pa\n", 3);
+		if((*lista)->fpos > (*lista)->next->fpos)
+		{
+			ft_sa(lista);
+			write(1, "sa\n", 3);
+		}
+		if((*lista)->next->fpos > (*lista)->next->next->fpos)
+		{
+			ft_ra(lista);
+			write(1, "ra\n", 3);
+			ft_sa(lista);
+			write(1, "sa\n", 3);
+			ft_rrb(lista);
+			write(1, "rrb\n", 4);
+		}
+	}
+	if((*listb)->fpos > (*lista)->next->fpos)
+	{
+		ft_pa(lista, listb);
+		write(1, "pa\n", 3);
+		if((*lista)->fpos > (*lista)->next->fpos)
+		{
+			ft_sa(lista);
+			write(1, "sa\n", 3);
+			ft_ra(lista);
+			write(1, "ra\n", 3);
+			ft_sa(lista);
+			write(1, "sa\n", 3);
+		}
+		ft_rra(lista);
+		write(1, "rra\n", 4);
+	}
+}
+
