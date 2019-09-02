c7d1d7574f7a7c68af16c95f513cc4e40febcccd
Gabriel Strauss <gstrauss@c4r18s3.wethinkcode.co.za>
2019-09-02 09:52:40 +0200

    stuff

--- perform.c
+++ perform.c
@@ -6,13 +6,13 @@
 /*   By: gstrauss <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/30 13:42:17 by gstrauss          #+#    #+#             */
-/*   Updated: 2019/08/30 13:42:24 by gstrauss         ###   ########.fr       */
+/*   Updated: 2019/09/02 09:42:40 by gstrauss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	perform(t_list **lista, t_list **listb, t_list *node)
+void	t1(t_list **lista, t_list **listb, t_list *node)
 {
 	if (ft_lstplen(*listb, node) > (ft_lstlen(*listb) / 2))
 	{
@@ -45,6 +45,10 @@ void	perform(t_list **lista, t_list **listb, t_list *node)
 			}
 		}
 	}
+}
+
+void	t2(t_list **lista, t_list **listb, t_list *node)
+{
 	if (ft_lstplen(*listb, node) <= (ft_lstlen(*listb) / 2))
 	{
 		while (*listb && node && *listb != node)
@@ -61,6 +65,23 @@ void	perform(t_list **lista, t_list **listb, t_list *node)
 				write(1, "rb\n", 3);
 		}
 	}
+}
+
+void	t3(t_list **lista, t_list **listb)
+{
+	if (ft_isgreater(*lista, *listb) == 1 && (*lista)->next)
+	{
+		ft_ra(lista);
+		write(1, "ra\n", 3);
+	}
+	ft_pa(lista, listb);
+	write(1, "pa\n", 3);
+}
+
+void	perform(t_list **lista, t_list **listb, t_list *node)
+{
+	t1(lista, listb, node);
+	t2(lista, listb, node);
 	if (ft_posdif(*lista, node) != 0)
 	{
 		if (ft_posdif(*lista, node) <= ft_lstlen(*lista) / 2)
@@ -80,11 +101,5 @@ void	perform(t_list **lista, t_list **listb, t_list *node)
 			}
 		}
 	}
-	if (ft_isgreater(*lista, *listb) == 1 && (*lista)->next)
-	{
-		ft_ra(lista);
-		write(1, "ra\n", 3);
-	}
-	ft_pa(lista, listb);
-	write(1, "pa\n", 3);
+	t3(lista, listb);
 }
